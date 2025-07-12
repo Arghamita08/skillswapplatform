76025adc27559e2d13169ed8d5fc8be0e423a5b2
Arghamita08 <147368055+Arghamita08@users.noreply.github.com>
2025-07-12 17:36:00 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from "react-router-dom"; 
+import { useUser } from '../context/UserContext';
 
 export default function Login() {
   const navigate = useNavigate(); 
