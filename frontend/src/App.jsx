b84b3e68baa5645720347628d354dc2eb970025b
Arghamita08 <147368055+Arghamita08@users.noreply.github.com>
2025-07-12 17:30:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Skills from './pages/Skills'
-import Swap from './pages/Swap'
-import Messages from './pages/Messages'
-import SignUp from './pages/SignUp'
-import Login from './pages/Login'
-import Navbar from './components/Navbar'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import { UserProvider } from './context/UserContext';
+import Profile from './pages/Profile';
+import Skills from './pages/Skills';
+import Swap from './pages/Swap';
+import Messages from './pages/Messages';
+import Navbar from './components/Navbar';
+import SignUp from './pages/Signup';
+import Login from './pages/Login'; 
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="p-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/swap" element={<Swap />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
+      <UserProvider>
+        <Navbar />
+        <div className="p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/swap" element={<Swap />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<div>Forgot Password Page Placeholder</div>} />
+          </Routes>
+        </div>
+      </UserProvider>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
